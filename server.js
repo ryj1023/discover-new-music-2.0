acff02b89e52c7d12f117ff0b2ae40663d9f5a82
Ryan Johnson <ryj1023@yahoo.com>
2017-04-01 11:30:35 -0400

    cleared and reinstalled node_modules

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 app.listen(process.env.PORT || 8080);
 console.log('server is running');
 
+
 var dir = '/Users/ryanjohnson/projects/Discover-The-Music-Heroku/'
 
 app.use(express.static(dir));
