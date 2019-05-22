60b14046a189c7860baf2224188c1f64b8a20db9
Ryan Johnson <ryj1023@yahoo.com>
2019-05-21 21:06:10 -0400

    update server,js

--- js/server.js
+++ js/server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 var path = require('path');
-var http = require('http');
+// var http = require('http');
 const axios = require('axios');
 var bodyParser = require('body-parser')
 
@@ -17,20 +17,20 @@ var dir = path.join(__dirname, '../')
 
 app.use(express.static(dir));
 
-app.post('/api/get-access-token', (req, res, next) => {
-   axios.post("https://accounts.spotify.com/api/token", {
-      "grant_type":    "authorization_code",
-     "redirect_uri":  "localhost:8080",
-     "client_secret": "b659abbe6d354267833920c37d88a499",
-     "client_id":     "1477dd268434476fa067b97c618573d5"})
-   .then(data => {
-      res.send('success')
-   })
-   .catch(err => {
-      console.log('err', err)
-      res.send(err)
-   })
-   });
+// app.post('/api/get-access-token', (req, res, next) => {
+//    axios.post("https://accounts.spotify.com/api/token", {
+//       "grant_type":    "authorization_code",
+//      "redirect_uri":  "localhost:8080",
+//      "client_secret": "b659abbe6d354267833920c37d88a499",
+//      "client_id":     "1477dd268434476fa067b97c618573d5"})
+//    .then(data => {
+//       res.send('success')
+//    })
+//    .catch(err => {
+//       console.log('err', err)
+//       res.send(err)
+//    })
+//    });
 
    app.post('/get-bands-event-data', (req, res) => {
       axios.get(`https://rest.bandsintown.com/artists/${req.body.data}/events?app_id=ryjay`)
